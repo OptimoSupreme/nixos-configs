9e0aee98a6457cf208e7851862ddfc1cff6d4ee0
Justin Ward <jward92@gmail.com>
2024-12-31 20:54:34 -0500

    bout did it

--- vics-pi/configuration.nix
+++ vics-pi/configuration.nix
@@ -1,124 +1,134 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
-  # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
-  boot.loader.grub.enable = false;
-  # Enables the generation of /boot/extlinux/extlinux.conf
-  boot.loader.generic-extlinux-compatible.enable = true;
-
-  networking.hostName = "goblin"; # Define your hostname.
-  # Pick only one of the below networking options.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-
-  # Set your time zone.
-  time.timeZone = "US/Eastern";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-
-  
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # hardware.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.justin = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      tree
-    ];
-    password = "password";
+  # Boot configuration
+  boot = {
+    loader= {
+      grub.enable = false;
+      generic-extlinux-compatible.enable = true;
+    };
+    kernel.sysctl = {
+      "net.ipv4.ip_forward" = 1;
+    };
   };
 
-  # programs.firefox.enable = true;
+# Networking
+  networking = {
+    hostName = "vics-pi";
+    firewall.enable = true;
+    nameservers = [ "1.1.1.1" ];
+    wireless = {
+      enable = true;
+      networks = {
+        "Airnet" = {
+          psk = "over9000";
+        };
+      };
+    };
+  };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
+  # Networking configuration
+  # networking = {
+  #   hostName = "vics-pi";
+  #   interfaces.enu1u1u1 = {
+  #     ipv4.addresses = [{
+  #       address = "192.168.0.11";
+  #       prefixLength = 24;
+  #     }];
+  #   };
+  #   defaultGateway = {
+  #     address = "192.168.0.1";
+  #     interface = "enu1u1u1";
+  #   };
+  #   nameservers = [ "1.1.1.1" ];
+  #   firewall.enable = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  # Time and locale settings
+  time.timeZone = "US/Eastern";
+  i18n.defaultLocale = "en_US.UTF-8";
 
-  # List services that you want to enable:
+  # Maintenance automation
+  system = {
+    autoUpgrade = {
+      enable = true;
+      dates = "Tue 03:00";
+      persistent = true;
+      allowReboot = true;
+    };
+    stateVersion = "24.11";
+  };
+  nix.gc = {
+    automatic = true;
+    dates = "Tue 03:30";
+    options = "--delete-older-than +7";
+    persistent = true;
+  };
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  # Modules and services
+  zramSwap = {
+    enable = true;
+    memoryPercent = 25;
+    priority = 5;
+  };
+   swapDevices = [ {
+    device = "/swapfile";
+    size = 4*1024;
+    priority = 1;
+  } ];
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  services = {
+    openssh.enable = true;
+    technitium-dns-server = {
+      enable = true;
+      openFirewall = true;
+    };
+    zoneminder = {
+      enable = true;
+      port = 8095;
+      storageDir = "/srv/security_cam";
+      openFirewall = true;
+      cameras = 2;
+      database = {
+        createLocally = true;
+        name = "zm";
+        username = "zoneminder";
+        password = "zmpass";
+        host = "localhost";
+      };
+    };
+  };
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
+  systemd.tmpfiles.rules = [
+    "d /srv/security_cam 0755 zoneminder nginx -"
+    "d /srv/security_cam/events 0755 zoneminder nginx -"
+    "d /srv/security_cam/exports 0755 zoneminder nginx -"
+    "d /srv/security_cam/images 0755 zoneminder nginx -"
+    "d /srv/security_cam/sounds 0755 zoneminder nginx -"
+  ];
 
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "24.11"; # Did you read the comment?
+  virtualisation.docker.enable = true;
 
+  # Environment and packages
+  nixpkgs.config.allowUnfree = true;
+  environment = {
+    systemPackages = with pkgs; [
+      git
+      wireguard-tools
+    ];
+  };
+
+  # User configuration
+  users.users = {
+    justin = {
+      isNormalUser = true;
+      description = "Justin";
+      extraGroups = [ "wheel" ];
+      password = "password";
+    };
+  };
 }
