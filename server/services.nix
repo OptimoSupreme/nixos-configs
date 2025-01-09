90edca7853ddba7b83c51386a8461a0b51839cfb
Justin Ward <jward92@gmail.com>
2025-01-09 11:20:09 -0500

    allow insecure dotnet-sdk.6.0

--- server/services.nix
+++ server/services.nix
@@ -78,6 +78,9 @@
     "d /srv/media/torrents 0770 media media - -"
     "d /srv/media/tv 0770 media media - -"
   ];
+  nixpkgs.config.permittedInsecurePackages = [
+    "dotnet-sdk-6.0.428"
+  ];
   services = {
     sonarr = {
       enable = true;
