5342c28b58647f9f36732b094873cbe928c7893a
Justin Ward <jward92@gmail.com>
2025-01-10 15:22:09 -0500

    fixed audiobookshelf permission

--- server/audiobookself.nix
+++ server/audiobookself.nix
@@ -4,6 +4,8 @@
   services = {
     audiobookshelf = {
       enable = true;
+      user = "media";
+      group = "media";
       openFirewall = true;
       port = 8000;
       host = "10.0.0.45";
