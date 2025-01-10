a121259ffed2bee517c2f65a5c8f9b2388244599
Justin Ward <jward92@gmail.com>
2025-01-10 15:28:18 -0500

    removing broken stuff for a good working main branch

--- server/media_server.nix
+++ server/media_server.nix
@@ -24,36 +24,6 @@
     "d /srv/media/qbittorrent 0770 media media - -"
   ];
 
-  # networking
-  networking = {
-    firewall.interfaces.enp2s0.allowedTCPPorts = [ 8080 ];
-    firewall.interfaces.enp2s0.allowedUDPPorts = [ 51820 ];
-    extraRoutingTables = {
-      qbittorrent = 200;
-    };
-    interfaces.qbittorrent0 = {
-      ipv4.addresses = [ { address = "10.0.50.1"; prefixLength = 24; } ];
-    };
-    routes = [
-      { table = "qbittorrent"; destination = "0.0.0.0/0"; interface = "mullvad0"; }
-    ];
-    wireguard.interfaces = {
-      mullvad0 = {
-        ips = [ "10.74.173.88/32" "fc00:bbbb:bbbb:bb01::b:ad57/128" ];
-        listenPort = 51820;
-        privateKeyFile = "/srv/secrets/wireguard-keys/mullvad_private";
-        peers = [
-          {
-            publicKey = "CsysTnZ0HvyYRjsKMPx60JIgy777JhD0h9WpbHbV83o=";
-            endpoint = "43.225.189.131:51820";
-            allowedIPs = [ "10.0.50.1/32" ];
-            persistentKeepalive = 25;
-          }
-        ];
-      };
-    };
-  };
-
   # applications
   environment.systemPackages = with pkgs; [ qbittorrent-nox jellyfin jellyfin-ffmpeg jellyfin-web ];
   nixpkgs.config.permittedInsecurePackages = [ "dotnet-sdk-6.0.428" "aspnetcore-runtime-6.0.36" ];
@@ -66,11 +36,7 @@
       ExecStart = "${pkgs.qbittorrent-nox}/bin/qbittorrent-nox";
       Restart = "on-failure";
       User = "media";
-      Environment = [
-        "HOME=/srv/media/qbittorrent"
-        "QBT_WEBUI_ADDRESS=10.0.0.45"
-        "QBT_CONNECTION_INTERFACE=qbittorrent0"
-      ];
+      Environment = [ "HOME=/srv/media/qbittorrent" ];
       WorkingDirectory = "/srv/media/qbittorrent";
       AmbientCapabilities= "CAP_NET_RAW";
     };
