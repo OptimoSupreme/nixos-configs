b23cb105eef0e9d4e2790fb32356af756bb741aa
Justin Ward <jward92@gmail.com>
2025-01-10 21:48:53 -0500

    media_server dev, definitely broken af

--- server/media_server.nix
+++ server/media_server.nix
@@ -27,50 +27,24 @@
   # networking
   networking = {
     firewall.interfaces.enp2s0.allowedTCPPorts = [ 8080 ];
-    firewall.interfaces.enp2s0.allowedUDPPorts = [ 51820 ];
-    extraRoutingTables = {
-      qbittorrent = 200;
-    };
-    interfaces.qbittorrent0 = {
-      ipv4.addresses = [ { address = "10.0.50.1"; prefixLength = 24; } ];
-    };
-    routes = [
-      { table = "qbittorrent"; destination = "0.0.0.0/0"; interface = "mullvad0"; }
-    ];
-    wireguard.interfaces = {
-      mullvad0 = {
-        ips = [ "10.74.173.88/32" "fc00:bbbb:bbbb:bb01::b:ad57/128" ];
-        listenPort = 51820;
-        privateKeyFile = "/srv/secrets/wireguard-keys/mullvad_private";
-        peers = [
-          {
-            publicKey = "CsysTnZ0HvyYRjsKMPx60JIgy777JhD0h9WpbHbV83o=";
-            endpoint = "43.225.189.131:51820";
-            allowedIPs = [ "10.0.50.1/32" ];
-            persistentKeepalive = 25;
-          }
-        ];
-      };
+    nat = {
+      enable = true;
+      internalInterfaces = [ "br0" "enp2s0" ];
+      routes = [
+        {
+          sourceAddress = "0.0.0.0/0";
+          sourcePort = 8080;
+          destination = "10.233.0.2";
+          destinationPort = 8080;
+        }
+      ];
     };
   };
 
   # applications
-  environment.systemPackages = with pkgs; [ qbittorrent-nox jellyfin jellyfin-ffmpeg jellyfin-web ];
+  environment.systemPackages = with pkgs; [ jellyfin jellyfin-ffmpeg jellyfin-web ];
   nixpkgs.config.permittedInsecurePackages = [ "dotnet-sdk-6.0.428" "aspnetcore-runtime-6.0.36" ];
   
-  systemd.services.qbittorrent = {
-    description = "qBittorrent-nox Service";
-    after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.qbittorrent-nox}/bin/qbittorrent-nox";
-      Restart = "on-failure";
-      User = "media";
-      Environment = [ "HOME=/srv/media/qbittorrent" ];
-      WorkingDirectory = "/srv/media/qbittorrent";
-      AmbientCapabilities= "CAP_NET_RAW";
-    };
-  };
   services = {
     sonarr = {
       enable = true;
@@ -119,6 +93,62 @@
       user = "media";
       group = "media";
       openFirewall = true;
+    };
+  };
+
+  # torrent + vpn container
+  containers.torbox = {
+    autoStart = true;
+    privateNetwork = true;
+    hostAddress = "10.233.0.1";
+    localAddress = "10.233.0.2";
+    config = { config, pkgs, ... }:
+    
+    {
+      # users and groups
+      users.groups.media = {};
+      users.users.media = {
+        isSystemUser = true;
+        group = "media";
+        shell = pkgs.bash;
+        home = "/srv/media/qbittorrent";
+      };
+
+      # networking
+        networking.defaultGateway = null;
+        networking.interfaces.eth0.ipv4.addresses = [{ address = "10.233.0.2"; prefixLength = 24; }];
+        networking.firewall.interfaces.mullvad0.allowedUDPPorts = [ 51820 ];
+        networking.wireguard.interfaces = {
+          mullvad0 = {
+            ips = [ "10.64.0.2/32" ]; # example; Mullvad might give you something else
+            privateKeyFile = "/etc/wireguard/mullvad-key"; # adjust accordingly
+            peers = [
+              {
+                publicKey = "MULLVAD_PUBLIC_KEY_HERE";
+                endpoint   = "1.2.3.4:51820";  # Mullvad server
+                allowedIPs = [ "0.0.0.0/0" ];
+                persistentKeepalive = 25;
+              }
+            ];
+          };
+        };
+
+      # applications
+      environment.systemPackages = with pkgs; [ qbittorrent-nox ];
+
+      systemd.services.qbittorrent = {
+        description = "qBittorrent-nox Service";
+        after = [ "network.target" ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          ExecStart = "${pkgs.qbittorrent-nox}/bin/qbittorrent-nox";
+          Restart = "on-failure";
+          User = "media";
+          Environment = [ "HOME=/srv/media/qbittorrent" ];
+          WorkingDirectory = "/srv/media/qbittorrent";
+          AmbientCapabilities= "CAP_NET_RAW";
+        };
+      };
     };
   };
 }
