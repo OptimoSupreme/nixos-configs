9014a4acaa3d50479485b395f7e696d073205141
Justin Ward <jward92@gmail.com>
2025-01-14 11:27:33 -0500

    first shairport commit attempt

--- server/shairport-sync.nix
+++ server/shairport-sync.nix
@@ -1,22 +1,80 @@
 { config, pkgs, ... }:
 
 {
+  # open firewall ports
+  networking.firewall = {
+    interfaces."enp2s0" = {
+      allowedTCPPorts = [
+        3689
+        5353
+        5000
+      ];
+      allowedUDPPorts = [
+        5353
+      ];
+      allowedTCPPortRanges = [
+        { from = 7000; to = 7001; }
+        { from = 32768; to = 60999; }
+      ];
+      allowedUDPPortRanges = [
+        { from = 319; to = 320; }
+        { from = 6000; to = 6009; }
+        { from = 32768; to = 60999; }
+      ];
+    };
+  };
+
+  # packages
   environment = {
     systemPackages = with pkgs; [
       alsa-utils
       shairport-sync-airplay2
     ];
   };
-  services = {
-    pipewire = {
+
+  # enable pipewire with alsa aupport
+  services.pipewire = {
+    enable = true;
+    alsa = {
       enable = true;
-      alsa = {
-        enable = true;
-        support32Bit = true;
-      };
-      pulse.enable = true;
+      support32Bit = true;
     };
   };
+
+  # setup resmaple for garbage  usb DAC compatibility :)
+  environment.etc."asound.conf".text = ''
+    # Resample for the outdoor speaker USB DAC
+    pcm.usb_dac1 {
+        type hw
+        card 1
+        device 0
+    }
+
+    pcm.resampled_dac1 {
+        type plug
+        slave {
+            pcm "usb_dac1"
+            rate 48000
+        }
+    }
+
+    # Resample for the dining room USB DAC
+    pcm.usb_dac2 {
+        type hw
+        card 2
+        device 0
+    }
+
+    pcm.resampled_dac2 {
+        type plug
+        slave {
+            pcm "usb_dac2"
+            rate 48000
+        }
+    }
+  '';
+
+  # systemd units
   systemd.services = {
     outdoor-speakers = {
       description = "Outdoor speakers shairport-sync instance";
