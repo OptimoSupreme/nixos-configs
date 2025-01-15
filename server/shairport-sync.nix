109dd24273bc417db5598a1b8d5f4dccd96aeaa8
Justin Ward <jward92@gmail.com>
2025-01-14 20:26:19 -0500

    back to pa!

--- server/shairport-sync.nix
+++ server/shairport-sync.nix
@@ -50,40 +50,6 @@
   # enable avahi
   # services.avahi.enable = true;
 
-  # setup resmaple for garbage  usb DAC compatibility :)
-  environment.etc."asound.conf".text = ''
-    # Resample for the outdoor speaker USB DAC
-    pcm.usb_dac1 {
-        type hw
-        card 1
-        device 0
-    }
-
-    pcm.resampled_dac1 {
-        type plug
-        slave {
-            pcm "usb_dac1"
-            rate 48000
-        }
-    }
-
-    # Resample for the dining room USB DAC
-    pcm.usb_dac2 {
-        type hw
-        card 2
-        device 0
-    }
-
-    pcm.resampled_dac2 {
-        type plug
-        slave {
-            pcm "usb_dac2"
-            rate 48000
-        }
-    }
-  '';
-
-  # systemd units
   systemd.services = {
     nqptp = {
       description = "Network Precision Time Protocol for Shairport Sync";
