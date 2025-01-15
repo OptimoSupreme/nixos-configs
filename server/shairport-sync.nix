d8911a8c9abfab14a5275468607b48f26068d2cf
Justin Ward <jward92@gmail.com>
2025-01-14 21:07:00 -0500

    testing the module again with pipewire

--- server/shairport-sync.nix
+++ server/shairport-sync.nix
@@ -1,44 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  # add shairport-sync user
-    users.users.shairport = {
-      description = "Shairport user";
-      isSystemUser = true;
-      createHome = true;
-      home = "/var/lib/shairport-sync";
-      group = "shairport";
-      extraGroups = [ "audio" ];
-    };
-    users.groups.shairport = {};
-  
-  # open firewall ports
-  networking.firewall = {
-    interfaces."enp2s0" = {
-      allowedTCPPorts = [
-        3689
-        5353
-        5000
-      ];
-      allowedUDPPorts = [
-        5353
-      ];
-      allowedTCPPortRanges = [
-        { from = 7000; to = 7001; }
-        { from = 32768; to = 60999; }
-      ];
-      allowedUDPPortRanges = [
-        { from = 319; to = 320; }
-        { from = 6000; to = 6009; }
-        { from = 32768; to = 60999; }
-      ];
-    };
-  };
-
   # packages
   environment = {
     systemPackages = with pkgs; [
-      alsa-utils
       nqptp
       shairport-sync-airplay2
     ];
@@ -53,9 +18,6 @@
     pulse.enable = true;
   };
 
-  # enable avahi
-  # services.avahi.enable = true;
-
   systemd.services = {
     nqptp = {
       description = "Network Precision Time Protocol for Shairport Sync";
@@ -67,23 +29,12 @@
         RestartSec = "5s";
       };
     };
-    outdoor-speakers = {
-      description = "Outdoor speakers shairport-sync instance";
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        User = "shairport";
-        Group = "shairport";
-        ExecStart = "${pkgs.shairport-sync}/bin/shairport-sync -c /srv/shairport-sync/outdoor_speakers.conf";
-      };
-    };
-    dining-room = {
-      description = "Dining room shairport-sync instance";
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        User = "shairport";
-        Group = "shairport";
-        ExecStart = "${pkgs.shairport-sync}/bin/shairport-sync -c /srv/shairport-sync/dining_room.conf";
-      };
-    };
+  };
+  
+  services.shairport-sync = {
+    enable = true;
+    openFirewall = true;
+    package = pkgs.shairport-sync-airplay2;
+    arguments = "-v -o pa";
   };
 }
