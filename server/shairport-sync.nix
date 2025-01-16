74035e625cea0c5541f63bec0b3caf72a1f9eb4a
Justin Ward <jward92@gmail.com>
2025-01-15 19:39:52 -0500

    shortened comment

--- server/shairport-sync.nix
+++ server/shairport-sync.nix
@@ -133,6 +133,6 @@
   '';
 }
 
-# run `sudo -u pulse PULSE_RUNTIME_PATH=/run/pulse pactl list sinks short` to display available sinks
+# run `sudo -u pulse pactl list sinks short` to display available sinks
 # run `sudo -u pulse alsamixer` to adjust volume levels
 # run `sudo alsactl store` so save the volume levels persistently
